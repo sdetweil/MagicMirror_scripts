7db7f664eeba05a52ce5230358af208ab80c2290
sam detweiler <sdetweil@gmail.com>
2021-03-15 16:41:34 -0500

    fix check for chromium

--- run-start.sh
+++ run-start.sh
@@ -78,10 +78,10 @@ else
       tee <&3 &
 			if [ $mac != 'Darwin' ]; then
         b="chromium"
-        if [ "($which $b)." == '.' ]; then
+        if [ "($which) $b." == '.' ]; then
           b='chromium-browser'
         fi
-        if [ "$(which $b)." != '.' ]; then
+        if [ "$(which) $b." != '.' ]; then
 				  "$b" -noerrdialogs -kiosk -start_maximized  --disable-infobars --app=http://localhost:$port  --ignore-certificate-errors-spki-list --ignore-ssl-errors --ignore-certificate-errors 2>/dev/null
         else
           echo "Chromium_browser not installed"
