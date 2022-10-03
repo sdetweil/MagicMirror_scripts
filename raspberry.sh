3c0806b8686bbfd423edae0cd6e7ab9f7b2e3b2e
Sam Detweiler <sdetweil@gmail.com>
2022-10-02 19:43:09 -0500

    update install for npm commandline problem

--- raspberry.sh
+++ raspberry.sh
@@ -414,10 +414,10 @@ if [ $doInstall == 1 ]; then
 	echo -e "\e[96mInstalling dependencies ...\e[90m" | tee -a $logfile
 	# check for NPM v8 or higher, changed parms for prod only on npm install
 	if [ ${NPM_CURRENT:1:1} -ge 8 ]; then
-		JustProd="--no-audit --no-fund --no-update-notifier --omit=dev"
+		JustProd="--no-audit --no-fund --no-update-notifier" 
 	fi
 	rm package-lock.json 2>/dev/null
-	npm_i_r=$(npm  $forced_arch $JustProd install)
+	npm_i_r=$(npm  $forced_arch $ --omit=dev install)
     npm_i_rc=$?
     # add the npm install messages to the logfile
   	echo $npm_i_r >> $logfile
