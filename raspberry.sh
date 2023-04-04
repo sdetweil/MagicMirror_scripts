204cad1f1d3b4438bdbfb68812b12dd4243d3fb8
Sam Detweiler <sdetweil@gmail.com>
2023-04-04 15:47:20 -0500

    update install to node 16.13.0

--- raspberry.sh
+++ raspberry.sh
@@ -26,7 +26,7 @@ doInstall=1
 true=1
 false=0
 # Define the tested version of Node.js.
-NODE_TESTED="v16.10.0"
+NODE_TESTED="v16.13.0"
 NPM_TESTED="V7.11.2"
 NODE_STABLE_BRANCH="${NODE_TESTED:1:2}.x"
 USER=`whoami`
