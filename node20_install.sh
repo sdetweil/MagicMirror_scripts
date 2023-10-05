b78ecf0eb3ad803f5de65936b35a8f6a1efbce4d
sam detweiler <sdetweil@gmail.com>
2023-10-05 12:05:31 -0500

    Update node20_install.sh
    add pin-priority

--- node20_install.sh
+++ node20_install.sh
@@ -20,7 +20,7 @@ sudo apt-get install -y ca-certificates curl gnupg
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
-echo "deb [$arch signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo "deb [$arch Pin-Priority=600 signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 sudo apt-get update
 sudo apt-get install nodejs -y
