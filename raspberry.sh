4f8f25152ae1c413d0ea8154cc03a36fa99b44b1
sam detweiler <sdetweil@gmail.com>
2023-10-11 12:40:51 -0500

    move screen saver setup in front of pm2 setup

--- raspberry.sh
+++ raspberry.sh
@@ -571,6 +571,113 @@ if command_exists plymouth; then
 else
 	echo -e "\e[93mplymouth is not installed.\e[0m" | tee -a $logfile
 fi
+# Disable Screensaver
+
+read -p "Do you want to disable the screen saver? (y/N)?" choice
+choice="${choice:-Y}"
+if [[ $choice =~ ^[Yy]$ ]]; then
+  # if this is a mac
+	if [ $mac == 'Darwin' ]; then
+	  # get the current setting
+	  setting=$(defaults -currentHost read com.apple.screensaver idleTime)
+		# if its on
+		if [ "$setting" != 0 ] ; then
+		  # turn it off
+			echo disable screensaver via mac profile >> $logfile
+			defaults -currentHost write com.apple.screensaver idleTime 0
+		else
+			echo mac profile screen saver already disabled >> $logfile
+		fi
+	else
+	  # find out if some screen saver running
+
+		# get just the running processes and args
+		# just want the program name (1st token)
+		# find the 1st with 'saver' in it (should only be one)
+		# parse with path char, get the last field ( the actual pgm name)
+
+	  screen_saver_running=$(ps -A -o args | awk '{print $1}' | grep -m1 [s]aver | awk -F\/ '{print $NF}');
+
+		# if we found something
+		if [ "$screen_saver_running." != "." ]; then
+		  # some screensaver running
+			case "$screen_saver_running" in
+			 mate-screensaver) echo 'mate screen saver' >>$logfile
+						gsettings set org.mate.screensaver lock-enabled false	 2>/dev/null
+						gsettings set org.mate.screensaver idle-activation-enabled false	 2>/dev/null
+						gsettings set org.mate.screensaver lock_delay 0	 2>/dev/null
+				 echo " $screen_saver_running disabled" >> $logfile
+				 DISPLAY=:0  mate-screensaver  >/dev/null 2>&1 &
+				 ;;
+			 gnome-screensaver) echo 'gnome screen saver' >>$logfile
+			   gnome_screensaver-command -d >/dev/null 2>&1
+				 echo " $screen_saver_running disabled" >> $logfile
+			   ;;
+			 xscreensaver) echo 'xscreensaver running' | tee -a $logfile
+			   xsetting=$(grep -m1 'mode:' ~/.xscreensaver )
+				 if [ $(echo $xsetting | awk '{print $2}') != 'off' ]; then
+					 sed -i "s/$xsetting/mode: off/" "$HOME/.xscreensaver"
+					 echo " xscreensaver set to off" >> $logfile
+				 else
+				   echo " xscreensaver already disabled" >> $logfile
+				 fi
+			   ;;
+			 gsd-screensaver | gsd-screensaver-proxy)
+					setting=$(gsettings get org.gnome.desktop.screensaver lock-enabled 2>/dev/null)
+					setting1=$(gsettings get org.gnome.desktop.session idle-delay 2>/dev/null)
+					if [ "$setting. $setting1." != '. .' ]; then
+						if [ "$setting $setting1" != 'false uint32 0' ]; then
+							echo "disable screensaver via gsettings was $setting and $setting1" >> $logfile
+							gsettings set org.gnome.desktop.screensaver lock-enabled false
+							gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
+							gsettings set org.gnome.desktop.session idle-delay 0
+						else
+							echo "gsettings screen saver already disabled" >> $logfile
+						fi
+					fi
+					;;
+			 *) echo "some other screensaver $screen_saver_running" found | tee -a $logfile
+			    echo "please configure it manually" | tee -a $logfile
+			   ;;
+		  esac
+		fi
+		if [ $(which gsettings | wc -l) == 1 ]; then
+			setting=$(gsettings get org.gnome.desktop.screensaver lock-enabled 2>/dev/null)
+			setting1=$(gsettings get org.gnome.desktop.session idle-delay 2>/dev/null)
+			if [ "$setting. $setting1." != '. .' ]; then
+				if [ "$setting $setting1" != 'false uint32 0' ]; then
+					echo "disable screensaver via gsettings was $setting and $setting1">> $logfile
+					gsettings set org.gnome.desktop.screensaver lock-enabled false
+					gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
+					gsettings set org.gnome.desktop.session idle-delay 0
+				else
+					echo "gsettings screen saver already disabled" >> $logfile
+				fi
+			fi
+		fi
+		if [ -e "/etc/lightdm/lightdm.conf" ]; then
+		  # if screen saver NOT already disabled?
+			if [ $(grep 'xserver-command=X -s 0 -dpms' /etc/lightdm/lightdm.conf | wc -l) == 0 ]; then
+			  echo "disable screensaver via lightdm.conf" >> $logfile
+				sudo sed -i '/^\[Seat:/a xserver-command=X -s 0 -dpms' /etc/lightdm/lightdm.conf
+			else
+			  echo "screensaver via lightdm already disabled" >> $logfile
+			fi
+		fi
+		if [ -d "/etc/xdg/lxsession/LXDE-pi" ]; then
+		  currently_set=$(grep -m1 '\-dpms' /etc/xdg/lxsession/LXDE-pi/autostart)
+			if [ "$currently_set." == "." ]; then
+				echo "disable screensaver via lxsession" >> $logfile
+				# turn it off for the future
+				sudo su -c "echo -e '@xset s noblank\n@xset s off\n@xset -dpms' >> /etc/xdg/lxsession/LXDE-pi/autostart"
+				# turn it off now
+				export DISPLAY=:0; xset s noblank;xset s off;xset -dpms
+			else
+			  echo "lxsession screen saver already disabled" >> $logfile
+			fi
+		fi
+	fi
+fi
 
 # Use pm2 control like a service MagicMirror
 read -p "Do you want use pm2 for auto starting of your MagicMirror (y/N)?" choice
@@ -703,113 +810,7 @@ if [[ $choice =~ ^[Yy]$ ]]; then
 		#$pm2cmd stop MagicMirror
 		pm2setup=$true
 fi
-# Disable Screensaver
 
-read -p "Do you want to disable the screen saver? (y/N)?" choice
-choice="${choice:-Y}"
-if [[ $choice =~ ^[Yy]$ ]]; then
-  # if this is a mac
-	if [ $mac == 'Darwin' ]; then
-	  # get the current setting
-	  setting=$(defaults -currentHost read com.apple.screensaver idleTime)
-		# if its on
-		if [ "$setting" != 0 ] ; then
-		  # turn it off
-			echo disable screensaver via mac profile >> $logfile
-			defaults -currentHost write com.apple.screensaver idleTime 0
-		else
-			echo mac profile screen saver already disabled >> $logfile
-		fi
-	else
-	  # find out if some screen saver running
-
-		# get just the running processes and args
-		# just want the program name (1st token)
-		# find the 1st with 'saver' in it (should only be one)
-		# parse with path char, get the last field ( the actual pgm name)
-
-	  screen_saver_running=$(ps -A -o args | awk '{print $1}' | grep -m1 [s]aver | awk -F\/ '{print $NF}');
-
-		# if we found something
-		if [ "$screen_saver_running." != "." ]; then
-		  # some screensaver running
-			case "$screen_saver_running" in
-			 mate-screensaver) echo 'mate screen saver' >>$logfile
-						gsettings set org.mate.screensaver lock-enabled false	 2>/dev/null
-						gsettings set org.mate.screensaver idle-activation-enabled false	 2>/dev/null
-						gsettings set org.mate.screensaver lock_delay 0	 2>/dev/null
-				 echo " $screen_saver_running disabled" >> $logfile
-				 DISPLAY=:0  mate-screensaver  >/dev/null 2>&1 &
-				 ;;
-			 gnome-screensaver) echo 'gnome screen saver' >>$logfile
-			   gnome_screensaver-command -d >/dev/null 2>&1
-				 echo " $screen_saver_running disabled" >> $logfile
-			   ;;
-			 xscreensaver) echo 'xscreensaver running' | tee -a $logfile
-			   xsetting=$(grep -m1 'mode:' ~/.xscreensaver )
-				 if [ $(echo $xsetting | awk '{print $2}') != 'off' ]; then
-					 sed -i "s/$xsetting/mode: off/" "$HOME/.xscreensaver"
-					 echo " xscreensaver set to off" >> $logfile
-				 else
-				   echo " xscreensaver already disabled" >> $logfile
-				 fi
-			   ;;
-			 gsd-screensaver | gsd-screensaver-proxy)
-					setting=$(gsettings get org.gnome.desktop.screensaver lock-enabled 2>/dev/null)
-					setting1=$(gsettings get org.gnome.desktop.session idle-delay 2>/dev/null)
-					if [ "$setting. $setting1." != '. .' ]; then
-						if [ "$setting $setting1" != 'false uint32 0' ]; then
-							echo "disable screensaver via gsettings was $setting and $setting1" >> $logfile
-							gsettings set org.gnome.desktop.screensaver lock-enabled false
-							gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
-							gsettings set org.gnome.desktop.session idle-delay 0
-						else
-							echo "gsettings screen saver already disabled" >> $logfile
-						fi
-					fi
-					;;
-			 *) echo "some other screensaver $screen_saver_running" found | tee -a $logfile
-			    echo "please configure it manually" | tee -a $logfile
-			   ;;
-		  esac
-		fi
-		if [ $(which gsettings | wc -l) == 1 ]; then
-			setting=$(gsettings get org.gnome.desktop.screensaver lock-enabled 2>/dev/null)
-			setting1=$(gsettings get org.gnome.desktop.session idle-delay 2>/dev/null)
-			if [ "$setting. $setting1." != '. .' ]; then
-				if [ "$setting $setting1" != 'false uint32 0' ]; then
-					echo "disable screensaver via gsettings was $setting and $setting1">> $logfile
-					gsettings set org.gnome.desktop.screensaver lock-enabled false
-					gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
-					gsettings set org.gnome.desktop.session idle-delay 0
-				else
-					echo "gsettings screen saver already disabled" >> $logfile
-				fi
-			fi
-		fi
-		if [ -e "/etc/lightdm/lightdm.conf" ]; then
-		  # if screen saver NOT already disabled?
-			if [ $(grep 'xserver-command=X -s 0 -dpms' /etc/lightdm/lightdm.conf | wc -l) == 0 ]; then
-			  echo "disable screensaver via lightdm.conf" >> $logfile
-				sudo sed -i '/^\[Seat:/a xserver-command=X -s 0 -dpms' /etc/lightdm/lightdm.conf
-			else
-			  echo "screensaver via lightdm already disabled" >> $logfile
-			fi
-		fi
-		if [ -d "/etc/xdg/lxsession/LXDE-pi" ]; then
-		  currently_set=$(grep -m1 '\-dpms' /etc/xdg/lxsession/LXDE-pi/autostart)
-			if [ "$currently_set." == "." ]; then
-				echo "disable screensaver via lxsession" >> $logfile
-				# turn it off for the future
-				sudo su -c "echo -e '@xset s noblank\n@xset s off\n@xset -dpms' >> /etc/xdg/lxsession/LXDE-pi/autostart"
-				# turn it off now
-				export DISPLAY=:0; xset s noblank;xset s off;xset -dpms
-			else
-			  echo "lxsession screen saver already disabled" >> $logfile
-			fi
-		fi
-	fi
-fi
 echo " "
 if [ $pm2setup -eq $true ]; then
 	rmessage="pm2 start MagicMirror"
