3314f67f9d2d7dc88ad9207452aba9a0410c7eda
sam detweiler <sdetweil@gmail.com>
2024-12-12 11:37:53 -0600

    Update raspberry.sh

--- raspberry.sh
+++ raspberry.sh
@@ -312,7 +312,7 @@ if [ $npminstalled == $false ]; then
 
 			# Check if a node process is currenlty running.
 			# If so abort installation.
-			node_running=$(ps -ef | grep node | grep -v grep)
+			node_running=$(ps -ef | grep node | grep -v grep | grep -v nodev) 
 			if [ "$node_running." != "." ]; then
 				echo -e "\e[91mA Node process is currently running. Can't upgrade." | tee -a $logfile
 				echo "Please quit all Node processes and restart the installer." | tee -a $logfile
