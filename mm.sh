a7fd87c46c070316436fcde238876acf3791082d
sam detweiler <sdetweil@gmail.com>
2025-03-29 08:11:25 -0700

    add option to run different start on different display engines

--- mm.sh
+++ mm.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # This file is still here to keep PM2 working on older installations.
 cd ~/MagicMirror
-DISPLAY=:0 npm start
+if [ $(ps -ef | grep -v grep | grep -e wayfire -e labwc | wc -l) -ne 0 ]; then 
+   WAYLAND_DISPLAY=wayland-1   
+   npm run start:wayland
+else
+   DISPLAY=:0 npm start
+fi   
