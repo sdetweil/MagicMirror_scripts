e52569354004f7cb71b4d70f9de1c0c00d85b620
sam detweiler <sdetweil@gmail.com>
2025-03-30 07:11:43 -0700

    remove extra comment from mm.sh

--- mm.sh
+++ mm.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# This file is still here to keep PM2 working on older installations.
 cd ~/MagicMirror
 if [ $(ps -ef | grep -v grep | grep -e wayfire -e labwc | wc -l) -ne 0 ]; then 
    WAYLAND_DISPLAY=wayland-1   
