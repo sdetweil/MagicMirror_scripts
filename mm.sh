06b790853dabc878cb9c0543a7d8e0b3067064c7
sam detweiler <sdetweil@gmail.com>
2025-03-30 10:35:42 -0700

    put mm.sh back to default

--- mm.sh
+++ mm.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 cd ~/MagicMirror
+'''
 if [ $(ps -ef | grep -v grep | grep -e wayfire -e labwc | wc -l) -ne 0 ]; then 
    WAYLAND_DISPLAY=wayland-1   
    npm run start:wayland
 else
    DISPLAY=:0 npm start
-fi   
+fi 
+'''
+DISPLAY=:0 npm start  
