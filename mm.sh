2f1095fb985475a05b8a22428661ee81022cbd79
sam detweiler <sdetweil@gmail.com>
2025-06-02 09:55:08 -0500

    set a default WAYLAND_DISPLAY if not set, package.json gets it wrong

--- mm.sh
+++ mm.sh
@@ -2,6 +2,8 @@
 cd ~/MagicMirror
 
 if [ $(ps -ef | grep -v grep | grep -i -e xway -e labwc | wc -l) -ne 0 ]; then 
+   # if WAYLAND_DISPLAYis set, use it, else set to -0
+   export WAYLAND_DISPLAY=${WAYLAND_DISPLAY:=wayland-0}
    npm run start:wayland
 else
    DISPLAY=:0 npm start
